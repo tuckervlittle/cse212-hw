210855876eb04f345a185d727e0c0a5a82cab489
tuckervlittle <tucker.little95@gmail.com>
2025-07-09 17:39:54 -1000

    Problem 1 completed

--- week02/code/PersonQueue.cs
+++ week02/code/PersonQueue.cs
@@ -13,7 +13,7 @@ public class PersonQueue
     /// <param name="person">The person to add</param>
     public void Enqueue(Person person)
     {
-        _queue.Insert(0, person);
+        _queue.Add(person); // Defect 1 was _queue.Insert(0, person)
     }
 
     public Person Dequeue()
--- week02/code/TakingTurnsQueue.cs
+++ week02/code/TakingTurnsQueue.cs
@@ -45,6 +45,11 @@ public class TakingTurnsQueue
                 person.Turns -= 1;
                 _people.Enqueue(person);
             }
+            // Defect 2 - There was no (else if) to accommodate for 0/negative (infinite) turns.
+            else if (person.Turns <= 0)
+            {
+                _people.Enqueue(person);
+            }
 
             return person;
         }
