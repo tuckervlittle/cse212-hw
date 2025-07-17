80cb2c5348c95b489a17e2ad65425e272027c819
tuckervlittle <tucker.little95@gmail.com>
2025-07-17 09:34:06 -1000

    Week 3 learning activity: Maps completed

--- week03/learn/Program.cs
+++ week03/learn/Program.cs
@@ -7,7 +7,13 @@ public class Program
         Console.WriteLine("\n======================\nDuplicate Counter\n======================");
         DuplicateCounter.Run();
 
+        Console.WriteLine("\n======================\nDuplicate Counter Solution\n======================");
+        DuplicateCounterSolution.Run();
+
         Console.WriteLine("\n======================\nTranslator\n======================");
         Translator.Run();
+
+        Console.WriteLine("\n======================\nTranslator Solution\n======================");
+        TranslatorSolution.Run();
     }
 }
